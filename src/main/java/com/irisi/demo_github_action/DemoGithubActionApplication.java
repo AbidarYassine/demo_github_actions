3b78bcb891258a57b2d63484a97fa2b1ec13b17e
AbidarYassine <yassine.abidar@edu.uca.ma>
2022-01-02 19:24:06 +0100

    init project

---
+++ src/main/java/com/irisi/demo_github_action/DemoGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.irisi.demo_github_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGithubActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/irisi/demo_github_action/DemoGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irisi.demo_github_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
